7b8ebb5192343d99694026b1403941b26962c753
mike2point71 <mpwright@gmail.com>
2022-10-08 17:56:06 -0700

    Update r0beccasecrets.sh

--- r0beccasecrets.sh
+++ r0beccasecrets.sh
@@ -4,7 +4,8 @@ ogogog () {
   aws secretsmanager --region=us-west-1 get-secret-value --secret-id=r0becca/passwords | jq -rc '.SecretString' >r0beccasecrets
   cat r0beccasecrets | jq -rc '."r0BECCA-key3-public"' >.ssh/r0BECCA-key3.pub
   cat r0beccasecrets | jq -rc '."r0BECCA-key3-private"' >.ssh/r0BECCA-key3
-  printf '%s\n  %s\n  %s\n' 'Host *' 'AddKeysToAgent yes' 'IdentityFile ~/.ssh/r0BECCA-key3' >~/.ssh/config
+  chmod 400 .ssh/r0BECCA-key3
+  printf '%s\n  %s\n  %s\n  %s\n' 'Host *' 'AddKeysToAgent yes' 'IdentityFile ~/.ssh/r0BECCA-key3' 'IdentitiesOnly yes' >~/.ssh/config
   rm r0beccasecrets
 }
 ogogog
